9dfd0a8b641a0d838990e3f9e42e32a4f0472e38
Ethan <ethan.blumenthal@gmail.com>
2021-05-05 18:13:28 -0500

    Added functionality to add transactions, persist state, and apply transactions to state.

--- database/state.go
+++ database/state.go
@@ -3,6 +3,7 @@ package database
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -39,7 +40,7 @@ func NewStateFromDisk() (*State, error) {
 	}
 
 	scanner := bufio.NewScanner(f)
-	state := &State{balances, make([]Tx, f)}
+	state := &State{balances, make([]Tx, 0), f}
 
 	// Iterate over each the tx.db file's line
 	for scanner.Scan() {
@@ -58,4 +59,53 @@ func NewStateFromDisk() (*State, error) {
 		}
 	}
 	return state, nil
+}
+
+func (s *State) Add(tx Tx) error {
+	if err := s.apply(tx); err != nil {
+		return err
+	}
+
+	s.txMempool = append(s.txMempool, tx)
+
+	return nil
+}
+
+func (s *State) Persist() error {
+	// Make a copy of mempool beacuse the s.txMempool will be modified
+	// in the loop below
+	mempool := make([]Tx, len(s.txMempool))
+	copy(mempool, s.txMempool)
+
+	for i := 0; i < len(mempool); i++ {
+		txJson, err := json.Marshal(mempool[i])
+		if err != nil {
+			return err
+		}
+
+		if _, err = s.dbFile.Write(append(txJson, '\n')); err != nil {
+			return err
+		}
+
+		// Remove the TX written to a file from the mempool
+		s.txMempool = s.txMempool[1:]
+	}
+
+	return nil
+}
+
+func (s *State) apply(tx Tx) error {
+	if tx.IsReward() {
+		s.Balances[tx.To] += tx.Value
+		return nil
+	}
+
+	if tx.Value > s.Balances[tx.From] {
+		return fmt.Errorf("insufficient balance")
+	}
+
+	s.Balances[tx.From] -= tx.Value
+	s.Balances[tx.To] += tx.Value
+
+	return nil
 }
