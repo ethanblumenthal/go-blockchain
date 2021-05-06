d72fc1d91a0621e01c82625ba5dc184c07923255
Ethan <ethan.blumenthal@gmail.com>
2021-05-05 22:26:19 -0500

    Added remward flag for transaction CLI.

--- cmd/blockchain/tx.go
+++ cmd/blockchain/tx.go
@@ -11,6 +11,7 @@ import (
 const flagFrom = "from"
 const flagTo = "to"
 const flagValue = "value"
+const flagData = "data"
 
 func txCmd() *cobra.Command {
 	var txsCmd = &cobra.Command{
@@ -35,11 +36,12 @@ func txAddCmd() *cobra.Command {
 			from, _ := cmd.Flags().GetString(flagFrom)
 			to, _ := cmd.Flags().GetString(flagTo)
 			value, _ := cmd.Flags().GetUint(flagValue)
+			data, _ := cmd.Flags().GetString(flagData)
 
 			fromAcc := database.NewAccount(from)
 			toAcc := database.NewAccount(to)
 
-			tx := database.NewTx(fromAcc, toAcc, value, "")
+			tx := database.NewTx(fromAcc, toAcc, value, data)
 
 			state, err := database.NewStateFromDisk()
 			if err != nil {
@@ -77,6 +79,8 @@ func txAddCmd() *cobra.Command {
 
 	cmd.Flags().Uint(flagValue, 0, "How many tokens to send")
 	cmd.MarkFlagRequired(flagValue)
+
+	cmd.Flags().String(flagData, "", "Possible values: 'reward'")
 
 	return cmd
 }
