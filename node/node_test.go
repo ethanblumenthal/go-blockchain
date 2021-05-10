8d428c4e1c364fe39d54c2b13bcadf02850b83de
Ethan <ethan.blumenthal@gmail.com>
2021-05-10 16:27:56 -0500

    Added mining end-to-end test.

--- node/node_test.go
+++ node/node_test.go
@@ -35,3 +35,52 @@ func TestNode_Run(t *testing.T) {
 		t.Fatal("node server was supposed to close after 5s")
 	}
 }
+
+func TestNode_Mining(t *testing.T) {
+	datadir := getTestDataDirPath()
+	err := fs.RemoveDir(datadir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Construct a new node instance where the TX originated from
+	nInfo := NewPeerNode("127.0.0.1", 8085, false, database.NewAccount(""), true)
+
+	// Construct a new node instance and configure Ethan as a miner
+	n := New(datadir, nInfo.IP, nInfo.Port, database.NewAccount("ethan"), nInfo)
+
+	// Allow the mining to run for 30mins at most
+	ctx, closeNode := context.WithTimeout(context.Background(), time.Minute*30)
+
+	// Schedule a new TX 3 seconds from now in a separate thread
+	go func() {
+		time.Sleep(time.Second * miningIntervalSeconds / 3)
+		tx := database.NewTx("ethan", "carley", 1, "")
+
+		// Add it to the Mempool
+		_ = n.AddPendingTX(tx, nInfo)
+	}()
+
+	go func() {
+		// Periodically check if we mined the 2 blocks
+		ticker := time.NewTicker(time.Second * 10)
+
+		for {
+			select {
+			case <- ticker.C:
+				// Close node if 2 blocks are mined as expected
+				if n.state.LatestBlock().Header.Number == 1 {
+					closeNode()
+					return
+				}
+			}
+		}
+	}()
+
+	_ = n.Run(ctx)
+
+	// Assert test result
+	if n.state.LatestBlock().Header.Number != 1 {
+		t.Fatal("2 pending TX not mined into 2 under 30min")
+	}
+}
